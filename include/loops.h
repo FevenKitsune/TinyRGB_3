cebd81f3d27694d183f0a731eb5b3053756d6b5e
Will Stewart <fevenkitsune@gmail.com>
2021-12-21 12:57:52 -0500

    added color blending function

--- include/loops.h
+++ include/loops.h
@@ -3,15 +3,17 @@
  *
  * Created: 12/06/2021 1:54:00 AM
  * Author : feven
- */ 
+ */
 
 #ifndef LOOPS_H
 #define LOOPS_H
 
 #include "tinyrgb.h"
+#include "colortools.h"
 #include <util/delay.h>
 
 void RGSweep();
 void RGBlink();
+void ColorBlend();
 
 #endif
